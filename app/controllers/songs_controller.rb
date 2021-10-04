dbf89eaccf41456cc88603434d171e76f493ea3e
Jelena <jelena.durovic@powerhrg.com>
2021-10-04 10:23:21 -0400

    commit tree 1d86497b15b5ef64e86707a43ad1244773b3fecc

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,35 @@
 class SongsController < ApplicationController
   def index
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist
+        @songs = @artist.songs
+      else
+        flash[:alert] = "Artist not found"
+        redirect_to artists_path   
+      end 
+    else
     @songs = Song.all
+    end 
   end
 
+
+
   def show
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+      if @song.nil? 
+        flash[:alert] = "Song not found"
+        redirect_to artist_songs_path
+      else
+        @song
+      end 
+    else   
     @song = Song.find(params[:id])
+    end 
   end
+
 
   def new
     @song = Song.new
